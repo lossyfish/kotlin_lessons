fcef33ef4b1315ef2a4fafc5c7901b9a64f23fe2
Vladislav Semchenok <totalkhl@mail.ru>
2023-08-20 19:58:44 +0500

    First_commit

---
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.test_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.test_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+
+            when(binding.editText.text.toString().toInt()){
+            in 0..200 -> {
+                binding.textView2.visibility = View.VISIBLE
+                binding.textView2.text = "You are a bad streamer"
+            }
+            in 201..500 -> {
+                binding.textView2.visibility = View.VISIBLE
+                binding.textView2.text = "You are a good streamer"
+            }
+            else -> {
+                binding.textView2.visibility = View.VISIBLE
+                binding.textView2.text = "You are a super star"
+            }
+            }
+        }
+    }
+}
